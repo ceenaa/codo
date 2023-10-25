bc38bb9b2af1b8b4b79b7b22768225fbd86099e0
sina <sina.moradi4233@gmail.com>
2023-10-25 14:54:53 +0330

    feat: adding user model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from .models import User, Rate
+
+admin.site.register(User)
+admin.site.register(Rate)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
 
 class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(unique=True)
+
+
+class Rate(models.Model):
+    date = models.DateField()
+    source = models.ForeignKey(User, on_delete=models.CASCADE, related_name='source')
+    destination = models.ForeignKey(User, on_delete=models.CASCADE, related_name='destination')
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
