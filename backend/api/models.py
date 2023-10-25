642c5017decc100c0ddb1db43dc8f23b06cdeb5a
Amirata051 <amirata.gh.m.n@gmail.com>
2023-10-25 15:34:32 +0330

    fix: model

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Mode):
+class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email_address = models.EmailField(unique=True)
